891a440718e17861de9cf68132733c004fcc6d5f
krstie <kristietran010308@gmail.com>
2024-09-04 11:05:09 -0700

    hello

--- problem1.java
+++ problem1.java
@@ -1,1 +1,9 @@
 // Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] agrs)
+    {
+    String favFood = "udon";
+    System.out.print("I like to eat " + favFood + " as well!");
+    }
+}
--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
 // Create your class here; don't forget the main method
+public class problem2
+{
+    public static void main(String[] agrs)
+    {
+    String name1 = "Kristie";
+    String name2 = "Emilia";
+    String name3 = "Thu";
+    String name4 = "Jade";
+    System.out.print(name1 + name2 + name3 + name4);
+    }
+}
